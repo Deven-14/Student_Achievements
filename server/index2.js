81b69d3e516adadc8d0c16e1dd20ebf48b6a9fd9
Deven-14 <deven.paramaj@gmail.com>
2022-01-02 17:24:37 +0530

    fixed create_batch and added all routes to admin

--- server/index2.js
+++ server/index2.js
@@ -2,13 +2,14 @@ const express = require('express');
 const global_data = require('./src/auth/global_data');
 const studentRouter = require('./src/routes/student_router');
 const adminRouter = require('./src/routes/admin_router');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 app.locals.departments = global_data.all_departments;
 // require('dotenv').config({ path: './server/.env' }); // path from the root of the module
-app.locals.dotenv = require('dotenv');
-app.locals.dotenv.config();
+// app.locals.dotenv = require('dotenv');
+// app.locals.dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
