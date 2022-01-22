fab9c4c7fffad9d43b3e5e94f2bbb8c552338013
Deven-14 <deven.paramaj@gmail.com>
2022-01-23 03:10:40 +0530

    changed expiry time for project review

--- server/src/helpers/token.js
+++ server/src/helpers/token.js
@@ -4,7 +4,7 @@ function generateAccessToken(id, email) {
     return jwt.sign(
         { user_id: id, email },
         process.env.ACCESS_TOKEN_SECRECT,
-        { expiresIn: "2m" }
+        { expiresIn: "30d" }
     );
 }
 
@@ -13,7 +13,7 @@ function generateRefreshToken(id, email) {
     return jwt.sign(
         { user_id: id, email },
         process.env.REFRESH_TOKEN_SECRECT,
-        { expiresIn: "8m" }
+        { expiresIn: "60d" }
     );
 }
 
